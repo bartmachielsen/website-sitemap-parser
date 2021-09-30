04688037f67c52def9bdc0086e039adca87448d0
Bart Machielsen <bartmachielsen@gmail.com>
2021-09-30 14:40:26 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='website-sitemap-parser',
+    version='0.0.1',
+    author="Bart Machielsen",
+    author_email="bartmachielsen@gmail.com",
+    description="Website Sitemap Parser",
+    install_requires=[
+        'requests'
+    ],
+    # long_description='',
+    # long_description_content_type="text/markdown",
+    url="https://github.com/bartmachielsen/website-sitemap-parser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
